8a44be6637ed9244934042e5840754384c108bfd
Weston Gibler <i-weston.gibler@fourwindsinteractive.com>
2018-08-17 14:19:54 -0600

    added animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
+import { Parallax } from 'react-spring'
+import './App.css'
+
+const Page = ({ offset, caption, first, second, gradient, onClick }) => (
+  <React.Fragment>
+    <Parallax.Layer offset={offset} speed={0.2} onClick={onClick}>
+      <div className="slopeBegin" />
+    </Parallax.Layer>
+
+    <Parallax.Layer offset={offset} speed={-0.2} onClick={onClick}>
+      <div className={`slopeEnd ${gradient}`} />
+    </Parallax.Layer>
+
+    <Parallax.Layer className="text number" offset={offset} speed={0.3}>
+      <span>0{offset + 1}</span>
+    </Parallax.Layer>
+
+    <Parallax.Layer className="text header" offset={offset} speed={0.4}>
+      <span>
+        <p style={{ fontSize: 20 }}>{caption}</p>
+        <div className={`stripe ${gradient}`} />
+        <p>{first}</p>
+        <p>{second}</p>
+      </span>
+    </Parallax.Layer>
+  </React.Fragment>
+)
+
+class App extends React.Component {
+  scroll = to => this.refs.parallax.scrollTo(to)
   render() {
     return (
-      <div className="App">
-       <h1>Fart!</h1>
-      </div>
-    );
+      <Parallax className="container" ref="parallax" pages={3} horizontal scrolling={false}>
+        <Page offset={0} gradient="pink" first="FA" second="RT" onClick={() => this.scroll(1)} />
+        <Page offset={1} gradient="teal" first="What does" second="That spell?" onClick={() => this.scroll(2)} />
+        <Page offset={2} gradient="tomato" first="fart!" onClick={() => this.scroll(0)} />
+      </Parallax>
+    )
   }
 }
 
